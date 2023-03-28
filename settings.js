454a1be0faf1afee7d7e1bd7fadfbd008ca9de9d
bri4nism <127416133+bri4nism@users.noreply.github.com>
2023-03-29 02:49:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://zenzapis.xyz': '162e5414e695',
 }
 
 
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6281320843460'] //ur owner number
+global.ownername = "Egi YTTA" //ur owner name
+global.ytname = "gapunya" //ur yt chanel name
+global.socialm = "Instagram: @moreegih" //ur github or insta name
+global.location = "Rusia, Chernobyl, Isekai" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['6281320843460']
+global.ownertag = '6281320843460' //ur tag number
+global.botname = 'Opet Stain' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.botscript = '' //script link
+global.reactmoji = "😋" //ur menu react emoji
+global.themeemoji = "😋" //ur theme emoji
+global.packname = "Opet wangsaff\n\n\n\n\n\n\n\n\n\n\n" //ur sticker watermark packname
+global.author = "Egi YTTA" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['6281320843460'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -102,23 +102,23 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','-']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    success: 'Done cuy',
+    admin: 'Fitur ini khusus Admin dek',
+    botAdmin: 'jadiin gweh admin dulu lah kocak',
+    owner: 'Fitur ini khusus sir Egi dek',
+    group: 'Fitur ini khusus di Grup dek',
+    private: 'Fitur ini khusus di Private chat dek',
+    bot: 'Fitur ini khusus gweh dek Opet ganteng no limit',
+    wait: 'Proses cuy',
+    linkm: 'Mana linknya kocak',
+    error: 'haduh error nih cuy',
+    endLimit: 'batas limit harian lu udah abis dek, tunggu 12 jam ok',
+    ban: 'lu dibanned kocak, sana chat sir Egi minta unbanned',
+    nsfw: 'Fitur NSFW (haram) blom aktif cuy, minta tolong Admin buat aktifin.',
+    banChat: 'Grup ini dibanned cuy, sana chat sir Egi minta unbanned'
 }
     global.limitawal = {
     premium: "Infinity",
